0c06544110020e5531a25eb2f7260e1d28958257
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-05-06 14:47:56 -0700

    Everything

---
+++ PrefabArmada/js/armada.js
@@ -0,0 +1,41 @@
+ArmadaSprite = function(imageName) {
+  // Call Sprite constructor
+  Phaser.Sprite.call(this, game, game.world.randomX, game.world.randomY, imageName);
+
+  // Define width, height, and velocity randomly
+  let scale_width = game.rnd.realInRange(0.25, 2.0);
+  let scale_height = game.rnd.realInRange(0.25, 2.0);
+  let velocity_x = game.rnd.between(5, 50);
+
+  // Set Properties
+  this.scale.setTo(scale_width, scale_height);
+  this.body.velocity.x = (-1 * velocity_x);
+
+  // Add [this] to existing game
+  game.add.existing(this);
+};
+
+armada_update = function() {
+  // Check for reversal
+  if (game.input.keyboard.justPressed(Phaser.Keyboard.R)) {
+    this.body.velocity.x *= -1;
+  }
+
+  // Set wrap padding - distance "beyond" x bounds that wrapping occurs
+  let wrap_padding = 10
+
+  // Check and wrap on left side
+  if (this.x < (0 - wrap_padding)) {
+    this.x = game.world.width;
+  }
+
+  // Check and wrap on right side
+  else if (this.x > (game.world.width + wrap_padding)) {
+    this.x = 0;
+  }
+};
+
+// JavaScript """""""Inheiritance""""""" boilerplate
+ArmadaSprite.prototype = Object.create(Phaser.Sprite.prototype);
+ArmadaSprite.prototype.constructor = ArmadaSprite;
+ArmadaSprite.prototype.update = armada_update;
--- PrefabArmada/js/main.js
+++ PrefabArmada/js/main.js
@@ -1,13 +1,24 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(960, 540, Phaser.AUTO, "", { preload: preload, create: create, update: update });
 
 function preload() {
-	// preload assets
+  game.load.image("teacher", "deepfried.png");
+  game.load.image("background", "oakes.png");
 }
 
 function create() {
-	// place your assets
+  // Build a classroom for the teacher - Oakes will do
+  game.add.sprite(0, 0, "background");
+
+  // Put the teachers in the classroom - maybe 50 will be enough to keep all the ragamuffins in line
+  addArmadaSprites(50, "teacher");
 }
 
 function update() {
-	// run game loop
+  // Today's Update Schedule: nothing
+}
+
+function addArmadaSprites(passedQuantity, passedKey) {
+  for (int count = 0; count < passedQuantity; count += 1) {
+    new ArmadaSprite(passedKey);
+  }
 }
